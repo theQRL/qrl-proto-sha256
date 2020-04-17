9cf31dc8c2ed2ccc2d5c6e506089d8d39694f522
JP Lomas <jp@theqrl.org>
2020-04-17 17:15:24 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,140 @@
+"use strict";
+exports.QRLPROTO_SHA256 = [
+  {
+    version: "1.0.0",
+    protoSha256: "4565ecb1a7e3852bd46a8e357bbcc95dfc7a81bec761df50550d567a71bf6ed6",
+    objectSha256: "d538cc0164f26cdda4e082cca548f531038d70a4b879495a4483f66e4c53cae9",
+  },
+  {
+    version: "1.1.0",
+    protoSha256: "9daaa59da125167ae825bf182a65c7f12a3af78f2cc351991a5faae03fb99892",
+    objectSha256: "7e841e796be583d30066e33f8d9b344ffe8160eb02fecc6cb4df80f7823e932c",
+  },
+  {
+    version: "1.1.1",
+    protoSha256: "9daaa59da125167ae825bf182a65c7f12a3af78f2cc351991a5faae03fb99892",
+    objectSha256: "7e841e796be583d30066e33f8d9b344ffe8160eb02fecc6cb4df80f7823e932c",
+  },
+  {
+    version: "1.1.2",
+    protoSha256: "9daaa59da125167ae825bf182a65c7f12a3af78f2cc351991a5faae03fb99892",
+    objectSha256: "7e841e796be583d30066e33f8d9b344ffe8160eb02fecc6cb4df80f7823e932c",
+  },
+  {
+    version: "1.1.3",
+    protoSha256: "9daaa59da125167ae825bf182a65c7f12a3af78f2cc351991a5faae03fb99892",
+    objectSha256: "7e841e796be583d30066e33f8d9b344ffe8160eb02fecc6cb4df80f7823e932c",
+  },
+  {
+    version: "1.1.4",
+    protoSha256: "71a51e5222c50a7575f1a92c365f6674bae938cebae678416da80f22fa8327b9",
+    objectSha256: "6589d425a16741104bbeceaa9ab2a1dbb33ff47453b90e29c3ee540dbad22df5",
+  },
+  {
+    version: "1.1.5",
+    protoSha256: "71a51e5222c50a7575f1a92c365f6674bae938cebae678416da80f22fa8327b9",
+    objectSha256: "6589d425a16741104bbeceaa9ab2a1dbb33ff47453b90e29c3ee540dbad22df5",
+  },
+  {
+    version: "1.1.6",
+    protoSha256: "71a51e5222c50a7575f1a92c365f6674bae938cebae678416da80f22fa8327b9",
+    objectSha256: "6589d425a16741104bbeceaa9ab2a1dbb33ff47453b90e29c3ee540dbad22df5",
+  },
+  {
+    version: "1.1.7 python",
+    protoSha256: "71a51e5222c50a7575f1a92c365f6674bae938cebae678416da80f22fa8327b9",
+    objectSha256: "6589d425a16741104bbeceaa9ab2a1dbb33ff47453b90e29c3ee540dbad22df5",
+  },
+  {
+    version: "1.1.8 python",
+    protoSha256: "71a51e5222c50a7575f1a92c365f6674bae938cebae678416da80f22fa8327b9",
+    objectSha256: "6589d425a16741104bbeceaa9ab2a1dbb33ff47453b90e29c3ee540dbad22df5",
+  },
+  {
+    version: "1.1.9 python",
+    protoSha256: "71a51e5222c50a7575f1a92c365f6674bae938cebae678416da80f22fa8327b9",
+    objectSha256: "6589d425a16741104bbeceaa9ab2a1dbb33ff47453b90e29c3ee540dbad22df5",
+  },
+  {
+    version: "1.1.10 python",
+    protoSha256: "00032d07d4b4637103db15b3d68ae019c14988e870475832af6eb5bd390e04f5",
+    objectSha256: "8c31491c3f6f7c3df96228644ded4ae5f3848fa74a971f3dfc8e4db684701dca",
+  },
+  {
+    version: "1.1.11 python",
+    protoSha256: "00032d07d4b4637103db15b3d68ae019c14988e870475832af6eb5bd390e04f5",
+    objectSha256: "8c31491c3f6f7c3df96228644ded4ae5f3848fa74a971f3dfc8e4db684701dca",
+  },
+  {
+    version: "1.1.13 python",
+    protoSha256: "df0f718b6a8a31ff3b746e74bcf461fe48c6d9ddac604826aa2b2891b4a7ed2a",
+    objectSha256: "a985aee431dae781576e3237f1a47d23ad579eacd5b9f4a6fddf6fd3833f1e12",
+  },
+  {
+    version: "1.1.15 python",
+    protoSha256: "df0f718b6a8a31ff3b746e74bcf461fe48c6d9ddac604826aa2b2891b4a7ed2a",
+    objectSha256: "a985aee431dae781576e3237f1a47d23ad579eacd5b9f4a6fddf6fd3833f1e12",
+  },
+  {
+    version: "1.1.15+65.gf434f320.dirty python",
+    protoSha256: "9c273e5aa3c88c8a225dd16a76203a95ca23f737599ebab991e1087badec7598",
+    objectSha256: "ffab214fb7f1497209b6a513c5a9053a0c5566741a91c836482e1057c0e57773",
+  },
+  {
+    version: "2.0.0+0.gbcf7644.dirty python",
+    protoSha256: "b24a5c41468610914da57ac92f3cc2f74d89575f2626d5968f84ca211f55410c",
+    objectSha256: "f6d7eb01dbb38707bb39d760619d05de01ae456a95060cb4849eafc3c358d12c",
+  },
+  {
+    version: "2.0.1+0.gf457b3a.dirty python",
+    protoSha256: "b24a5c41468610914da57ac92f3cc2f74d89575f2626d5968f84ca211f55410c",
+    objectSha256: "f6b06eac25bd5740fe6877d5e604429e6aadd93dee668e7412051decbf34add2",
+  },
+  {
+    version: "2.0.1+0.gf457b3a.dirty python (null)",
+    protoSha256: "b24a5c41468610914da57ac92f3cc2f74d89575f2626d5968f84ca211f55410c",
+    objectSha256: "f6b06eac25bd5740fe6877d5e604429e6aadd93dee668e7412051decbf34add2",
+  },
+  {
+    version: "2.0.0+0.gbcf7644.dirty python (null)",
+    protoSha256: "b24a5c41468610914da57ac92f3cc2f74d89575f2626d5968f84ca211f55410c",
+    objectSha256: "f6d7eb01dbb38707bb39d760619d05de01ae456a95060cb4849eafc3c358d12c",
+  },
+  {
+    version: "2.0.0 python",
+    protoSha256: "b24a5c41468610914da57ac92f3cc2f74d89575f2626d5968f84ca211f55410c",
+    objectSha256: "f6d7eb01dbb38707bb39d760619d05de01ae456a95060cb4849eafc3c358d12c",
+  },
+  {
+    version: "2.0.0 python (null)",
+    protoSha256: "b24a5c41468610914da57ac92f3cc2f74d89575f2626d5968f84ca211f55410c",
+    objectSha256: "f6d7eb01dbb38707bb39d760619d05de01ae456a95060cb4849eafc3c358d12c",
+  },
+  {
+    version: "2.0.1 python",
+    protoSha256: "b24a5c41468610914da57ac92f3cc2f74d89575f2626d5968f84ca211f55410c",
+    objectSha256: "f6b06eac25bd5740fe6877d5e604429e6aadd93dee668e7412051decbf34add2",
+  },
+  {
+    version: "2.0.1 python (null)",
+    protoSha256: "b24a5c41468610914da57ac92f3cc2f74d89575f2626d5968f84ca211f55410c",
+    objectSha256: "f6b06eac25bd5740fe6877d5e604429e6aadd93dee668e7412051decbf34add2",
+  },
+  // development .proto
+  {
+    version: "0+unknown python",
+    protoSha256: "9d035851c76143621960c858d064489838f6eef664930257946a4b23d70bcc6c",
+    objectSha256: "7e062340104109d2206017bbdb5cddf45386f77d8dd169d08fa9624b5c6b2934",
+  },
+  {
+    version: "0.2.0 python (null)",
+    protoSha256: "ab168a46875c6dc41000080804c858b9039aae977dfd5503d9eb234ece4e3510",
+    objectSha256: "0ead495c340fd331b28e325166ffe162a0f7cc3929b559f47d3691aeb0541bae",
+  },
+  // testnet public release
+  {
+    version: "0.2.0 python",
+    protoSha256: "ab168a46875c6dc41000080804c858b9039aae977dfd5503d9eb234ece4e3510",
+    objectSha256: "0ead495c340fd331b28e325166ffe162a0f7cc3929b559f47d3691aeb0541bae",
+  },
+];
---
+++ test.js
@@ -0,0 +1,40 @@
+var assert = require('assert');
+var QRLP = require('./index.js').QRLPROTO_SHA256;
+
+var CURRENT_PROTOS = 27;
+
+describe('QRLPROTO_SHA256', function() {
+  describe('-> length', function() {
+    it('should contain ' + CURRENT_PROTOS + ' hashes', function() {
+      assert.equal(QRLP.length, CURRENT_PROTOS);
+    });
+    it('should not contain 0 hashes', function() {
+      assert.notEqual(QRLP.length, 0);
+    });
+  });
+  describe('-> structure', function() {
+    var versions = 0;
+    var protoSha256 = 0;
+    var objectSha256 = 0;
+    QRLP.forEach(function (element) {
+      if (element.version.length > 0) {
+        versions += 1;
+      };
+      if (element.protoSha256.length === 64) {
+        protoSha256 += 1;
+      };
+      if (element.objectSha256.length === 64) {
+        objectSha256 += 1;
+      };
+    });
+    it('each element has a version string', function() {
+      assert.equal(versions, CURRENT_PROTOS);
+    });
+    it('each element has a valid length proto SHA256 checksum', function() {
+      assert.equal(protoSha256, CURRENT_PROTOS);
+    });
+    it('each element has a valid length object SHA256 checksum', function() {
+      assert.equal(objectSha256, CURRENT_PROTOS);
+    });
+  });
+});
